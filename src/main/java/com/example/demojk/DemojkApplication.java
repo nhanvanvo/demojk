36de4efcc5067f0276ce851b224dca8e9d32fba8
Nhan Vo <nhanvo@devsecops.com.vn>
2021-03-14 15:38:51 +0700

    Initial commit

---
+++ src/main/java/com/example/demojk/DemojkApplication.java
@@ -0,0 +1,13 @@
+package com.example.demojk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemojkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemojkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demojk/DemojkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demojk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemojkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
